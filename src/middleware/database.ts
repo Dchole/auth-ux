c94b8fe56830d2072800ed369d248f297525e40e
Dchole <derekoware47@gmail.com>
2021-01-24 16:24:05 +0000

    mongo uri

--- src/middleware/database.ts
+++ src/middleware/database.ts
@@ -2,15 +2,10 @@ import { Db, MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
 
-const client = new MongoClient(
-  process.env.NODE_ENV === "production"
-    ? process.env.MONGODB_URI
-    : process.env.MONGODB_LOCAL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 export interface IReq extends NextApiRequest {
   dbClient: MongoClient;
@@ -18,10 +13,14 @@ export interface IReq extends NextApiRequest {
 }
 
 async function database(req: IReq, _res: NextApiResponse, next: any) {
-  if (!client.isConnected())
+  if (!client.isConnected()) {
+    console.log(client.isConnected());
     await client.connect().then(() => console.log("Connected to DB!"));
+  } else {
+    console.log("Already Connected");
+  }
   req.dbClient = client;
-  req.db = client.db("MCT");
+  req.db = client.db("auth");
   return next();
 }
 
